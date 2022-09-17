c502294aa9475dc5619bf157c234234333d7421d
gustavofalcao1 <gustavo.falcao01@gmail.com>
2022-09-17 12:06:04 +0100

    upfate app name

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
 export default function App() {
-  LogBox.ignoreLogs(['AsyncStorage has been extracted from react-native core'])
+  LogBox.ignoreLogs(["AsyncStorage has been extracted from react-native core and will be removed in a future release. It can now be installed and imported from '@react-native-async-storage/async-storage' instead of 'react-native'. See https://github.com/react-native-async-storage/async-storage"])
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
