1d33fdd5ce196983b12f1e1b2713d098df4974be
Gustavo Falcão <gustavo.falcao01@gmail.com>
2025-03-24 13:22:11 +0000

    Corrigir configuração do babel para usar react-native-dotenv corretamente

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      ['module:babel-plugin-dotenv', {
+      ['module:react-native-dotenv', {
         moduleName: '@env',
         path: '.env',
         blacklist: null,
